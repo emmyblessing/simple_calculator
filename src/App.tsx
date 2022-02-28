ad176258e4b20d9d3c0a3442f75ebcf74189acf4
emmyblessing <emmyblessing78@gmail.com>
2022-02-28 10:52:20 +0100

    project initialization

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Initialize Project
         </a>
       </header>
     </div>
